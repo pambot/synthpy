f8192498bea3b23d26d338b7262fcd9428cb9959
pambot <thefourtheye@gmail.com>
2018-02-11 14:43:50 -0500

    some setup for upload to pypi

---
+++ setup.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(
+  name = 'synthpy',
+  packages = ['synthpy'], 
+  version = '0.1',
+  description = 'A library for the generation of synthetic data using the Python scientific stack',
+  author = 'Pamela Wu',
+  author_email = 'thefourtheye@gmail.com',
+  url = 'https://github.com/pambot/synthpy', 
+  download_url = 'https://github.com/pambot/synthpy/archive/0.1.tar.gz', 
+  keywords = ['synthetic data'], 
+  classifiers = [],
+)
